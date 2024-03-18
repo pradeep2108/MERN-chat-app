7a92f64b1348b013c4ac9bdfc081a19fc1dacfbe
pradeep2108 <Pradeepr0821@gmail.com>
2024-03-18 17:53:49 +0530

    start of chat application

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const port = process.env.PORT || 8000;
+const { chats } = require("../backend/data/dummydata");
+const app = express();
+dotenv.config();
+
+app.get("/api/chat/data", (req, res) => {
+  res.status(200).json(chats);
+});
+
+app.listen(port, () =>
+  console.log(`server started successfully on port ${port}`)
+);
