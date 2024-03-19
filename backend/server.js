93eb25dcc1ebe766ab6c2c39001efc549185cb5a
pradeep2108 <Pradeepr0821@gmail.com>
2024-03-19 22:27:29 +0530

    created a react app

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 6000;
 const { chats } = require("../backend/data/dummydata");
 const app = express();
 dotenv.config();
---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">Hello Gang</div>;
+}
+
+export default App;
