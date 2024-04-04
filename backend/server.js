2b4531e25c8483f414e38b5cdb5a4ab1a3aedc7b
pradeep2108 <Pradeepr0821@gmail.com>
2024-04-04 13:54:33 +0530

    created a DB connection using Mongo Atlas

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv");
 const port = process.env.PORT || 6000;
 const { chats } = require("../backend/data/dummydata");
-const app = express();
+const connectDB = require("./config/db");
 dotenv.config();
+connectDB();
+const app = express();
 
 app.get("/api/chat/data", (req, res) => {
   res.status(200).json(chats);
--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,7 +1,74 @@
-import React from "react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => setShow(!show);
+  const submitHandler = () => {};
+
+  const getGuestCredentials = () => {
+    setEmail("guest@buzzchat.com");
+    setPassword("root");
+  };
+  console.log(email + " " + password);
+  return (
+    <VStack>
+      <FormControl id="loginEmail" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          _placeholder="Enter Your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="loginPassword" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            type={show ? "text" : "password"}
+            _placeholder="Enter Your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem" onClick={handleClick}>
+            <Button h="1.75rem" size="sm">
+              {show ? "Hide" : "show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="50%"
+        marginTop={"20px"}
+        borderRadius={"40px"}
+        onClick={submitHandler}
+        margin={"flex"}
+      >
+        Login
+      </Button>
+      <Button
+        colorScheme="orange"
+        width="50%"
+        marginTop={"20px"}
+        borderRadius={"40px"}
+        onClick={getGuestCredentials}
+      >
+        Get Guest Credentials
+      </Button>
+    </VStack>
+  );
 };
 
 export default Login;
